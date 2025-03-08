c1dc1ae7fd2bc101916154cc6aa4c72d2bb59a75
vamshi2401 <vamshi1718@gmail.com>
2025-03-08 19:20:35 +0530

    recipe details

--- routes/recipes.js
+++ routes/recipes.js
@@ -121,4 +121,19 @@ router.get('/search', async (req, res) => {
   }
 });
 
+//get recipe by id
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+
+    if (!recipe) {
+      return res.status(404).json({ error: "Recipe not found" });
+    }
+
+    res.json(recipe);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 module.exports = router;
